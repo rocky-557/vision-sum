273a43136bbe6151fee9796152c8958a5346f2e0
Sri Raghav <92212389+rocky-557@users.noreply.github.com>
2024-11-09 16:32:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-git clone https://github.com/rocky-557/vision-sum.git
 cd vision-sum
 python -m venv env
 chmod +x env/bin/activate
 source env/bin/activate
-pip install torch torchvision transformers einops mistralrs pillow 
+pip3 install torch torchvision transformers einops mistralrs pillow 
 clear
 echo "Everything has been Setup Completely. Now run md.py using 'python3 md.py'."
