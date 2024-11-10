29dc92e18b7b58e06ac943cb6c226df109469026
Sri Raghav <92212389+rocky-557@users.noreply.github.com>
2024-11-10 14:26:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 python3 -m venv env
 chmod +x env/bin/activate
 source env/bin/activate
-pip3 install torch torchvision transformers einops pillow 
+pip3 install torch torchvision transformers einops pillow flask
 clear
-echo "Everything has been Setup Completely. Now run md.py using 'python3 md.py'."
+echo "Everything has been Setup Completely. Now run md.py using 'python3 app.py'."
